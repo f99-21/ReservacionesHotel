372cbfcf82b92032d4c2fad0e02f791f60372648
f99-21 <sandovalfabri@gmail.com>
2024-08-13 14:52:12 -0600

    segundo commit

---
+++ src/main/java/Entidade/Persona.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidade;
+
+/**
+ *
+ * @author sando
+ */
+public class Persona {
+    
+    String Nombre;
+    String Apellido;
+    int Dui;
+    int Telefono;
+    Datetime fecha;
+    
+}
